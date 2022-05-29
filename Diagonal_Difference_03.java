b743214c3fc3e7a6f352a0e3a704de304492a111
Valentin Terziev <valentinterziev9701@gmail.com>
2022-05-29 20:21:19 +0300

    Add files via upload

---
+++ Diagonal_Difference_03.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Diagonal_Difference_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+        int[][] matrix = new int[size][size];
+
+        for (int row = 0; row < size; row++) {
+            String[] rowInput = scanner.nextLine().split(" ");
+            for (int col = 0; col < size; col++) {
+                matrix[row][col] = Integer.parseInt(rowInput[col]);
+            }
+        }
+
+        int mainDiagSum = 0;
+        for (int counter = 0; counter < size; counter++) {
+            mainDiagSum += matrix[counter][counter];
+        }
+
+        int secondDiagSum = 0;
+
+//        for (int row = 0, col = size - 1; row < size; row++, col--) {
+//            secondDiagSum +=matrix[row][col];
+//        }
+
+        for (int row = 0; row < size; row++) {
+            secondDiagSum +=matrix[row][size - row - 1];
+        }
+
+        int result = Math.abs(mainDiagSum - secondDiagSum);
+        System.out.println(result);
+    }
+}
---
+++ Matrix_OF_Palindromes_02.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matrix_OF_Palindromes_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] input = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int rows = input[0];
+        int cols = input[1];
+
+        String[][] matrix = new String[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                char outside = (char) ('a' + row);
+                char middle = (char) (outside + col);
+                matrix[row][col] = String.valueOf(outside) + middle + outside;
+            }
+        }
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
